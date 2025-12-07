8e740ce63e9bafe9b36b59390a3c084b3f9c73b2
wahidul <wahidulhassan60@gmail.com>
2025-12-08 03:22:24 +0600

    2nd

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,21 @@
 import express, { Request, Response } from "express"
-import {Pool} from "pg";
+import { Pool } from "pg";
 import dotenv from 'dotenv'
 import path from 'path'
 
 
 const app = express()
-const port = 3000
+const port = 5000
 
 dotenv.config({
-  path : path.join(process.cwd(), ".env")
-  
+  path: path.join(process.cwd(), ".env")
 })
 
 const pool = new Pool({
-  connectionString : `${process.env.CONNECTION_STR}`
+  connectionString: `${process.env.CONNECTION_STR}`
 })
 
-const initDB = async()=>{
+const initDB = async () => {
   await pool.query(`
       CREATE TABLE IF NOT EXISTS users (
       id SERIAL PRIMARY KEY,
@@ -29,7 +28,7 @@ const initDB = async()=>{
       updated_at TIMESTAMP DEFAULT NOW()
       )
     `);
-    await pool.query(`
+  await pool.query(`
         CREATE TABLE IF NOT EXISTS todos(
           id SERIAL PRIMARY KEY,
           user_id INT REFERENCES users(id) ON DELETE CASCADE,
@@ -48,19 +47,46 @@ initDB();
 app.use(express.json())
 
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!') 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
 })
 
-app.post('/post', (req:Request, res:Response)=>{
-  console.log(req.body);
-  res.status(200).json({
-    message : true,
-    sussces : "Posted Method",
-  }); 
+//User crud
+app.post('/user', async (req: Request, res: Response) => {
+  const { name, email } = req.body
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email]
+    );
+    res.status(201).json({
+      success: true,
+      message: "message sent"
+    })
+  } catch (err: any) {
+    res.status(500).json({
+      successful: false,
+      message: err.message
+    })
+  }
+})
+
+app.get('/users/:id', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id]);
+    console.log(req.params.id)
+    res.status(201).json({
+      success: true,
+      message : "successful"
+    })
+  } catch (err:any) {
+    res.status(500).json({
+      success: false,
+      message : err.message
+    })
+  }
   
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)  
+  console.log(`Example app listening on port ${port}`)
 })
