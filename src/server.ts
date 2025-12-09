bbf6fe1d41dad63542e7787cce13073327da31c2
WAHIDUL <wahidlhassan60@gmail.com>
2025-12-09 17:35:25 +0600

    Middleware logger done

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express"
 import { Pool } from "pg";
 import dotenv from 'dotenv'
 import path from 'path'
@@ -10,6 +10,8 @@ const port = 5000
 dotenv.config({
   path: path.join(process.cwd(), ".env")
 })
+
+app.use(express.json())
 
 const pool = new Pool({
   connectionString: `${process.env.CONNECTION_STR}`
@@ -44,7 +46,7 @@ const initDB = async () => {
 
 initDB();
 
-app.use(express.json())
+
 
 
 app.get('/', (req: Request, res: Response) => {
@@ -60,11 +62,28 @@ app.post('/users', async (req: Request, res: Response) => {
     );
     res.status(201).json({
       success: true,
-      message: "message sent"
+      message: "user created"
     })
   } catch (err: any) {
     res.status(500).json({
       successful: false,
+      message: err.message
+    })
+  }
+})
+
+app.get("/allusers", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`)
+    console.log(req)
+    res.status(200).json({
+      success: true,
+      message: "all users shown below",
+      data: result.rows
+    })
+  } catch (err: any) {
+    res.status(404).json({
+      success: false,
       message: err.message
     })
   }
@@ -88,20 +107,20 @@ app.get('/users/:id', async (req: Request, res: Response) => {
 })
 
 app.put('/update/:id', async (req: Request, res: Response) => {
-  const {name, email} = req.body
+  const { name, email } = req.body
   try {
     const result = await pool.query(`UPDATE users SET name = $1, email=$2 WHERE id=$3 RETURNING*`, [name, email, req.params.id]);
 
-    if(result.rows.length === 0){
+    if (result.rows.length === 0) {
       res.status(201).json({
         success: false,
         message: "data not found"
       })
-    }else{
-        res.status(201).json({
+    } else {
+      res.status(201).json({
         success: true,
         message: "updated successfully",
-        data : result.rows[0]
+        data: result.rows[0]
       })
     }
 
@@ -114,37 +133,91 @@ app.put('/update/:id', async (req: Request, res: Response) => {
 
 })
 
-app.delete("/delete/:id", async (req:Request, res:Response)=> {
-  try{
+app.delete("/delete/:id", async (req: Request, res: Response) => {
+  try {
     const result = await pool.query(`DELETE FROM users WHERE id = $1 RETURNING *`, [req.params.id]);
-    if(result.rows.length === 0){
+    console.log(result.rowCount)
+    if (result.rowCount === 0) {
       res.status(404).json({
-        successful : false,
-        message : "data not found"
+        successful: false,
+        message: "data not found"
       })
-    }else{
+    } else {
       res.status(201).json({
-          success : true,
-       message : "deleted successfully"
+        success: true,
+        message: "deleted successfully"
       })
-     
+
     }
-  }catch(err:any){
+  } catch (err: any) {
     res.status(404).json({
-      success : false,
-      message : err.message
+      success: false,
+      message: err.message
     })
   }
 })
 
-app.post("/todos", async (req:Request, res:Response)=> {
-  const [title] = req.body;
-  try{
-    const result = await pool.query(`INSERT INTO todos(title)`, [title])
-  }catch(err:any){
-
+app.post("/todos", async (req: Request, res: Response) => {
+  const { title, user_id } = req.body;
+  console.log(req.body)
+  try {
+    const result = await pool.query(`INSERT INTO todos(title, user_id) VALUES($1, $2) RETURNING *`, [title, user_id])
+    res.status(201).json({
+      success: true,
+      messaage: "created todos",
+      data: result.rows
+    })
+  } catch (err: any) {
+    res.status(201).json({
+      success: false,
+      message: err.message
+    })
   }
 })
+
+app.delete("/deletetodo/:id", async (req: Request, res: Response) => {
+  const result = await pool.query(`DELETE FROM todos WHERE id = $1 RETURNING*`, [req.params.id])
+  try {
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success : false,
+        Message : "Data not found"
+      })
+    }else{
+      res.status(201).json({
+        success : true,
+        message : "Data deleted",
+        data : result.rows
+      })
+    }
+
+  } catch (err: any) {
+      res.status(404).json({
+        success : false, 
+        message : err.message,
+        data : result.rows
+      })
+  }
+})
+
+const logger = (req : Request, res : Response, next : NextFunction)=> {
+  console.log(`Date : ${new Date().toISOString()}`);
+  next()
+}
+
+app.get("/logger", logger, (req, res)=>{
+  res.send(`${logger} hello wahidul this is loger middleware function`)
+})
+
+app.use((req, res)=>{  
+  res.status(404).json({
+    success : false,
+    message : "route not found",
+    path : req.path
+  })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
