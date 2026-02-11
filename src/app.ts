26f8f657fa8d556f292e4211501e70c34389f6d4
Wahidul <wahidulhassan60@gmail.com>
2026-02-11 15:02:15 +0600

    final

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,22 @@
-import express, { Request, Response } from "express"
-import initDB, { pool } from "./config/db";
-import logger from "./middleware/logger";
+import express from "express"
+import initDB from "./config/db";
 import { userRoutes } from "./modules/user/user.routes";
 import { todoRoutes } from "./modules/todo/todos.routes";
 import { authRoute } from "./modules/auth/auth.routes";
 
 const app = express()
 
-app.use(express.json())
+//request.body not idefined
+app.use(express.json()) 
+
 initDB();
+
+//main route
 app.use("/users", userRoutes);
 app.use("/todos", todoRoutes);
 app.use("/auth", authRoute)
 
+//default route
 app.use((req, res) => {
   res.status(404).json({
     success: false,
